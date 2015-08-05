615c549c4e71f395e44ede0752fe40a12d22d51b
Dmitriy Zyryanov <zyryanov.dmitriy@wildberries.ru>
2015-08-05 12:31:53 +0300

    h kakaya-to

--- MoreTranglez!/Program.cs
+++ MoreTranglez!/Program.cs
@@ -8,24 +8,6 @@ namespace MoreTranglez_
 		static void Main()
 		{
 			NextPythagoreanTriple(534, 712, 890);
-			var products = new [] {
-new Product { Name = new String("Chai"), Price = 18.00m },
-new Product { Name = "Chang", Price = 19.00m },
-new Product { Name = "Aniseed Syrup", Price = 10.00m },
-new Product { Name = "Chef Anton’s Cajun Seasoning", Price = 22.00m },
-new Product { Name = "Chef Anton’s Gumbo Mix", Price = 21.35m },
-new Product { Name = "Grandma’s Boysenberry Spread", Price = 25.00m },
-new Product { Name = "Uncle Bob’s Organic Dried Pears", Price = 30.00m },
-new Product { Name = "Northwoods Cranberry Sauce", Price = 40.00m },
-new Product { Name = "Mishi Kobe Niku", Price = 97.00m },
-new Product { Name = "Ikura", Price = 31.00m }
-};
-		}
-
-		private class Product
-		{
-			public string Name;
-			public decimal Price;
 		}
 
 		private static void NextPythagoreanTriple(int a, int b, int c)
